c97d960af255ea594bf090ab0f380e08cd7d3801
Johannestt <johannes.tornemothoren@elev.ga.lbs.se>
2018-09-28 14:59:07 +0200

    NIce
    en gåva till dig

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    public int dragonhealth;
+    public int playerhealth;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+       // dragonhealth += Random.Range(100 - 151);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) ;
+        {
+            print(dragonhealth);
+
+
+        }
+
+
+    }
+}
---
+++ Assets/testinguppgift.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testinguppgift : MonoBehaviour
+{
+
+    public int userValue;
+    public int starterValue;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void task2()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            starterValue += Random.Range(1, 7);
+            starterValue -= Random.Range(1, 7);
+        }
+
+
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        task2();
+
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                print(starterValue);
+
+                if (starterValue <= 0)
+                {
+                    print("You lose");
+                }
+
+                if (starterValue >= 20)
+                {
+                    print("You Win");
+
+
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
